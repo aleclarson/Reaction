04587422fae4dc4e7d5ab2dac041b436a2d6a598
aleclarsoniv <alec.stanford.larson@gmail.com>
2015-10-22 20:15:23 -0700

    10/22/2015

--- js/src/Reaction.js
+++ js/src/Reaction.js
@@ -1,8 +1,10 @@
-var Immutable, NamedFunction, Reaction, ReactiveVar, Tracker, define, isType, ref, setType, steal, sync;
+var Factory, Immutable, Reaction, ReactiveVar, Tracker, Void, assertType, define, emptyFunction, isType, ref, setType, sync;
 
-ref = require("type-utils"), setType = ref.setType, isType = ref.isType;
+require("lotus-require");
 
-NamedFunction = require("named-function");
+ref = require("type-utils"), setType = ref.setType, isType = ref.isType, assertType = ref.assertType, Void = ref.Void;
+
+emptyFunction = require("emptyFunction");
 
 ReactiveVar = require("reactive-var");
 
@@ -10,144 +12,155 @@ Immutable = require("immutable");
 
 sync = require("io").sync;
 
+Factory = require("factory");
+
 Tracker = require("tracker");
 
 define = require("define");
 
-steal = require("steal");
-
-Reaction = NamedFunction("Reaction", function(options) {
-  var reaction;
-  reaction = setType({}, Reaction);
-  if (isType(options, Function)) {
-    options = {
-      get: options
-    };
-  }
-  define(reaction, function() {
-    this.options = {};
-    this({
-      keyPath: options.keyPath,
-      value: {
-        get: function() {
-          if (this._computation === Tracker.currentComputation) {
-            return this.__getValue();
-          }
-          return this._value.get();
-        },
-        set: function(newValue) {
-          return this._value.set(newValue);
-        }
-      }
-    });
-    this.enumerable = false;
-    this({
-      _value: ReactiveVar(),
+module.exports = Reaction = Factory("Reaction", {
+  initArguments: function(config, context) {
+    assertType(config, [Object, Function], "config");
+    if (isType(config, Function)) {
+      config = {
+        get: config
+      };
+    }
+    return [config, context];
+  },
+  optionTypes: {
+    keyPath: [String, Void],
+    get: [Function],
+    didSet: [Function, Void],
+    didChange: [Function, Void]
+  },
+  initValues: function(config, context) {
+    return {
+      keyPath: config.keyPath,
       _willNotify: false,
       _stopped: true,
       _computation: null,
       _listeners: Immutable.Set(),
-      _sync: steal(options, "_sync", false)
-    });
-    this.frozen = true;
-    return this({
-      __getNewValue: options.get
-    });
-  });
-  if (isType(options.didSet, Function)) {
-    reaction.addListener(options.didSet);
-  }
-  if (isType(options.didChange, Function)) {
-    reaction.addListener(function(newValue, oldValue) {
-      if (reaction._computation.firstRun || (newValue === oldValue)) {
-        return;
+      _context: context
+    };
+  },
+  customValues: {
+    value: {
+      get: function() {
+        if (this._computation === Tracker.currentComputation) {
+          return this.__getValue();
+        }
+        return this._value.get();
+      },
+      set: function(newValue) {
+        return this._value.set(newValue);
       }
-      return options.didChange(newValue, oldValue);
-    });
-  }
-  if (Reaction.autoStart) {
-    reaction.start();
-  }
-  return reaction;
-});
-
-define(Reaction, function() {
-  this.options = {};
-  this({
-    autoStart: true
-  });
-  this.frozen = true;
-  return this({
-    sync: function(options) {
-      options._sync = true;
-      return Reaction(options);
     }
-  });
-});
-
-define(Reaction.prototype, function() {
-  this.options = {
-    frozen: true
-  };
-  this({
-    start: function() {
-      if (!this._stopped) {
-        return;
-      }
-      this._stopped = false;
-      Tracker.autorun((function(_this) {
-        return function() {
-          var newValue, oldValue;
-          _this._computation = Tracker.currentComputation;
-          oldValue = _this.__getValue();
-          newValue = _this.__getNewValue();
-          return Tracker.nonreactive(function() {
-            _this.value = newValue;
-            if (_this._sync || _this._computation.firstRun) {
-              return _this._notifyListeners();
-            }
-            if (_this._willNotify) {
-              return;
-            }
-            _this._willNotify = true;
-            return Tracker.afterFlush(function() {
-              _this._willNotify = false;
-              return _this._notifyListeners();
-            });
-          });
+  },
+  initFrozenValues: function(config) {
+    return {
+      _value: ReactiveVar(),
+      _sync: config._sync != null ? config._sync : false,
+      __getNewValue: config.get
+    };
+  },
+  init: function(config) {
+    if (config.didSet != null) {
+      this.addListener((function(_this) {
+        return function(newValue, oldValue) {
+          return config.didSet.call(_this._context, newValue, oldValue);
         };
       })(this));
-    },
-    stop: function() {
-      if (this._stopped) {
-        return;
-      }
-      this._stopped = true;
-      this._computation.stop();
-      this._computation = null;
-    },
-    addListener: function(listener) {
-      this._listeners = this._listeners.add(listener);
-    },
-    removeListener: function(listener) {
-      this._listeners = this._listeners["delete"](listener);
     }
-  });
-  this.enumerable = false;
-  return this({
-    __getValue: function() {
-      return this._value.curValue;
-    },
-    _notifyListeners: function(oldValue) {
-      return this._listeners.forEach(this._notifyListener.bind(this, this.value, oldValue));
-    },
-    _notifyListener: function(newValue, oldValue, listener) {
-      return listener(newValue, oldValue);
-    },
-    _run: function() {}
-  });
+    if (config.didChange != null) {
+      this.addListener((function(_this) {
+        return function(newValue, oldValue) {
+          if (_this._computation.firstRun || (newValue === oldValue)) {
+            return;
+          }
+          return config.didChange.call(_this._context, newValue, oldValue);
+        };
+      })(this));
+    }
+    if (Reaction.autoStart) {
+      return this.start();
+    }
+  },
+  initFactory: function() {
+    return this.autoStart = true;
+  },
+  start: function() {
+    if (!this._stopped) {
+      return;
+    }
+    this._stopped = false;
+    Tracker.autorun((function(_this) {
+      return function() {
+        var newValue, oldValue;
+        _this._computation = Tracker.currentComputation;
+        oldValue = _this.__getValue();
+        newValue = _this.__getNewValue.call(_this._context);
+        return Tracker.nonreactive(function() {
+          _this.value = newValue;
+          if (_this._sync || _this._computation.firstRun) {
+            return _this._notifyListeners(oldValue);
+          }
+          if (_this._willNotify) {
+            return;
+          }
+          _this._willNotify = true;
+          return Tracker.afterFlush(function() {
+            _this._willNotify = false;
+            return _this._notifyListeners(oldValue);
+          });
+        });
+      };
+    })(this));
+  },
+  stop: function() {
+    if (this._stopped) {
+      return;
+    }
+    this._stopped = true;
+    this._computation.stop();
+    this._computation = null;
+  },
+  addListener: function(listener) {
+    this._listeners = this._listeners.add(listener);
+  },
+  removeListener: function(listener) {
+    this._listeners = this._listeners["delete"](listener);
+  },
+  fork: function(config) {
+    var transform;
+    assertType(config, [Object, Function]);
+    transform = emptyFunction.thatReturnsArgument;
+    if (isType(config, Function)) {
+      transform = config;
+      config = {};
+    }
+    config.get = (function(_this) {
+      return function() {
+        return transform(_this.value);
+      };
+    })(this);
+    return Reaction(config);
+  },
+  statics: {
+    sync: function(config) {
+      config._sync = true;
+      return Reaction(config);
+    }
+  },
+  __getValue: function() {
+    return this._value.curValue;
+  },
+  _notifyListeners: function(oldValue) {
+    return this._listeners.forEach(this._notifyListener.bind(this, this.value, oldValue));
+  },
+  _notifyListener: function(newValue, oldValue, listener) {
+    return listener.call(this, newValue, oldValue);
+  }
 });
-
-module.exports = Reaction;
 
 //# sourceMappingURL=../../map/src/Reaction.map
--- src/Reaction.coffee
+++ src/Reaction.coffee
@@ -1,119 +1,125 @@
 
-{ setType, isType } = require "type-utils"
-NamedFunction = require "named-function"
+require "lotus-require"
+
+{ setType, isType, assertType, Void } = require "type-utils"
+emptyFunction = require "emptyFunction"
 ReactiveVar = require "reactive-var"
 Immutable = require "immutable"
 { sync } = require "io"
+Factory = require "factory"
 Tracker = require "tracker"
 define = require "define"
-steal = require "steal"
 
-Reaction = NamedFunction "Reaction", (options) ->
+module.exports =
+Reaction = Factory "Reaction",
 
-  reaction = setType {}, Reaction
+  initArguments: (config, context) ->
+    assertType config, [ Object, Function ], "config"
+    config = { get: config } if isType config, Function
+    [ config, context ]
 
-  if isType options, Function
-    options = get: options
+  optionTypes:
+    keyPath: [ String, Void ]
+    get: [ Function ]
+    didSet: [ Function, Void ]
+    didChange: [ Function, Void ]
 
-  define reaction, ->
+  initValues: (config, context) ->
+    keyPath: config.keyPath
+    _willNotify: no
+    _stopped: yes
+    _computation: null
+    _listeners: Immutable.Set()
+    _context: context
 
-    @options = {}
-    @
-      keyPath: options.keyPath
-      value:
-        get: ->
-          # Prevent infinite loops. The reaction should not depend on its own value.
-          return @__getValue() if @_computation is Tracker.currentComputation
-          @_value.get()
-        set: (newValue) ->
-          @_value.set newValue
+  customValues:
 
-    @enumerable = no
-    @
-      _value: ReactiveVar()
-      _willNotify: no
-      _stopped: yes
-      _computation: null
-      _listeners: Immutable.Set()
-      _sync: steal options, "_sync", no
+    value: {
+      get: ->
+        # Prevent infinite loops. The reaction should not depend on its own value.
+        return @__getValue() if @_computation is Tracker.currentComputation
+        @_value.get()
 
-    @frozen = yes
-    @
-      __getNewValue: options.get
+      set: (newValue) ->
+        @_value.set newValue
+    }
 
-  if isType options.didSet, Function
-    reaction.addListener options.didSet
+  initFrozenValues: (config) ->
+    _value: ReactiveVar()
+    _sync: if config._sync? then config._sync else no
+    __getNewValue: config.get
 
-  if isType options.didChange, Function
-    reaction.addListener (newValue, oldValue) ->
-      return if reaction._computation.firstRun or (newValue is oldValue)
-      options.didChange newValue, oldValue
+  init: (config) ->
 
-  reaction.start() if Reaction.autoStart
+    if config.didSet?
+      @addListener (newValue, oldValue) =>
+        config.didSet.call @_context, newValue, oldValue
 
-  reaction
+    if config.didChange?
+      @addListener (newValue, oldValue) =>
+        return if @_computation.firstRun or (newValue is oldValue)
+        config.didChange.call @_context, newValue, oldValue
 
-define Reaction, ->
+    @start() if Reaction.autoStart
 
-  @options = {}
-  @
-    autoStart: yes
+  initFactory: ->
+    @autoStart = yes
 
-  @frozen = yes
-  @
-    sync: (options) ->
-      options._sync = yes
-      Reaction options
+  start: ->
+    return unless @_stopped
+    @_stopped = no
+    Tracker.autorun =>
+      @_computation = Tracker.currentComputation
+      oldValue = @__getValue()
+      newValue = @__getNewValue.call @_context
+      Tracker.nonreactive =>
+        @value = newValue
+        if @_sync or @_computation.firstRun
+          return @_notifyListeners oldValue
+        if @_willNotify
+          return
+        @_willNotify = yes
+        Tracker.afterFlush =>
+          @_willNotify = no
+          @_notifyListeners oldValue
+    return
 
-define Reaction.prototype, ->
+  stop: ->
+    return if @_stopped
+    @_stopped = yes
+    @_computation.stop()
+    @_computation = null
+    return
 
-  @options = frozen: yes
-  @
-    start: ->
-      return unless @_stopped
-      @_stopped = no
-      Tracker.autorun =>
-        @_computation = Tracker.currentComputation
-        oldValue = @__getValue()
-        newValue = @__getNewValue()
-        Tracker.nonreactive =>
-          @value = newValue
-          return @_notifyListeners() if @_sync or @_computation.firstRun
-          return if @_willNotify
-          @_willNotify = yes
-          Tracker.afterFlush =>
-            @_willNotify = no
-            @_notifyListeners()
-      return
+  addListener: (listener) ->
+    @_listeners = @_listeners.add listener
+    return
 
-    stop: ->
-      return if @_stopped
-      @_stopped = yes
-      @_computation.stop()
-      @_computation = null
-      return
+  removeListener: (listener) ->
+    @_listeners = @_listeners.delete listener
+    return
 
-    addListener: (listener) ->
-      @_listeners = @_listeners.add listener
-      return
+  fork: (config) ->
+    assertType config, [ Object, Function ]
+    transform = emptyFunction.thatReturnsArgument
+    if isType config, Function
+      transform = config
+      config = {}
+    config.get = => transform @value
+    Reaction config
 
-    removeListener: (listener) ->
-      @_listeners = @_listeners.delete listener
-      return
+  statics:
 
-  @enumerable = no
-  @
-    # Avoids triggering Tracker.Dependency.depend()
-    __getValue: ->
-      @_value.curValue
+    sync: (config) ->
+      config._sync = yes
+      Reaction config
 
-    _notifyListeners: (oldValue) ->
-      @_listeners.forEach @_notifyListener.bind this, @value, oldValue
+  # Avoids triggering Tracker.Dependency.depend()
+  __getValue: ->
+    @_value.curValue
 
-    _notifyListener: (newValue, oldValue, listener) ->
-      listener newValue, oldValue
+  _notifyListeners: (oldValue) ->
+    @_listeners.forEach @_notifyListener.bind this, @value, oldValue
 
-    _run: ->
-
-
-module.exports = Reaction
+  _notifyListener: (newValue, oldValue, listener) ->
+    listener.call this, newValue, oldValue
